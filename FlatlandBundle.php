88b8887e6138f8ebf46c540de6f6a89240ee0bd8
Ruben van der Linde <ruben@conduction.nl>
2020-06-27 23:24:14 +0200

    First Code

---
+++ FlatlandBundle.php
@@ -0,0 +1,20 @@
+<?php
+
+// Conduction/FlatlandBundle/FlatlandBundle.php
+
+/*
+ * This file is part of the Conduction Flatland
+ *
+ * (c) Conduction <info@conduction.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Conduction\FlatlandBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FlatlandBundle extends Bundle
+{
+}
